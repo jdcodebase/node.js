59ca72ee1c790bf4a49f42df829d20926feedbbe
Jatin Dhamija <jd.codebase@gmail.com>
2026-03-09 12:01:49 +0530

    03-Node.js-Modules

---
+++ 03-Node.js-Modules/app.js
@@ -0,0 +1,8 @@
+const math = require("./math");
+const utils = require("./utils");
+const fs = require("fs");
+
+console.log(math.add(2, 3));
+console.log(math.subtract(10, 5));
+console.log(utils.greet("John"));
+console.log(utils.square(5));
---
+++ 03-Node.js-Modules/utils.js
@@ -0,0 +1,12 @@
+function greet(name) {
+  return `Hello ${name}`;
+}
+
+function square(num) {
+  return num * num;
+}
+
+module.exports = {
+  greet,
+  square,
+};
